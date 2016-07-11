987c6e5affa4ac50f43d4757170d2be1a0fd980f
Star <tingxiaz@andrew.cmu.edu>
2016-07-11 22:37:15 +0800

    [258. Add Digits][Accepted]committed by LeetCode Extension

---
+++ 258-Add-Digits/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def addDigits(self, num):
+        result = 0
+        if num >= 10:
+            while result == 0 or result >= 10:
+                if result != 0:
+                    num = result
+                    result = 0
+                while num/10 > 0:
+                    result += num % 10
+                    num = num/10
+                result += num
+            return result
+        else:
+            return num
+        
+        
+        """
+        :type num: int
+        :rtype: int
+        """
