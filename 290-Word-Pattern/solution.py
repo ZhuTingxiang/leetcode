7c9338fac45ee36aec7cd24a8380360960f27354
Star <tingxiaz@andrew.cmu.edu>
2016-07-29 22:40:54 +0800

    [290. Word Pattern][Runtime Error]committed by LeetCode Extension

--- 290-Word-Pattern/solution.py
+++ 290-Word-Pattern/solution.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def wordPattern(self, pattern, str):
+        print str
         str_split = []
         for i in str.split():
             if i is not None:
                 str_split.append(i)
         for n in range(0,len(pattern)-1):
             for m in range(n+1,len(pattern)):
-                print n 
-                print m
                 if pattern[n] == pattern[m]:
-                    
                     if str_split[n] != str_split[m]:
                         return False
                 else:
