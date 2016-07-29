7c5ef9dd2beb6860b04dd67e7a30e2bbadfe671d
Star <tingxiaz@andrew.cmu.edu>
2016-07-29 23:46:17 +0800

    [374. Guess Number Higher or Lower][Accepted]committed by LeetCode Extension

---
+++ 374-Guess-Number-Higher-or-Lower/solution.py
@@ -0,0 +1,32 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        print n
+        g=(1+n)/2
+        result = guess(g)
+        small_num = 1
+        big_num = n
+        if guess(n) == 0:
+            return n
+        else:
+            while result!=0:
+                if result == -1:
+                    big_num = g
+                    g = (g+small_num)/2
+                elif result == 1:
+                        small_num = g
+                        g=(big_num+g)/2
+                    
+                result = guess(g)
+            return g
+
+
+        
+        """
+        :type n: int
+        :rtype: int
+        """
