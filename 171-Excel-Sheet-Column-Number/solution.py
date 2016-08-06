1d5abcf2c2c5f7a4e611f553f443540904fb7af4
Star <tingxiaz@andrew.cmu.edu>
2016-08-07 00:00:03 +0800

    [171. Excel Sheet Column Number][Accepted]committed by LeetCode Extension

--- 171-Excel-Sheet-Column-Number/solution.py
+++ 171-Excel-Sheet-Column-Number/solution.py
@@ -5,11 +5,10 @@ class Solution(object):
         :rtype: int
         """
         result = 1
-        if len(s) == 1:
-            result = ord(s[0]) % ord('A') + 1
-        else:
-            result = 0
-            for i in range(0,len(s)):
-                result += 26**(len(s)-i-1)*(ord(s[i]) % ord('A')+1)
-                print "i,result",i,result 
+        # if len(s) == 1:
+        #     result = ord(s[0]) % ord('A') + 1
+        # else:
+        result = 0
+        for i in range(0,len(s)):
+            result += 26**(len(s)-i-1)*(ord(s[i]) % ord('A')+1)
         return result
