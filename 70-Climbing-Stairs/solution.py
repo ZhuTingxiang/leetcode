1547103206c968f3d84a2fe679d5f91aa337cbc6
Star <tingxiaz@andrew.cmu.edu>
2016-08-07 22:50:47 +0800

    [70. Climbing Stairs][Accepted]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.py
+++ 70-Climbing-Stairs/solution.py
@@ -6,10 +6,16 @@ class Solution(object):
         """
         if n < 1:
             return 0
-        elif n == 1 :
+        elif n == 1:
             return 1
         elif n == 2:
             return 2
         else:
-            return self.climbStairs(n-1)+self.climbStairs(n-2)
-        
+            a, b = 1, 2
+            num = 3
+            while num <= n:
+                temp = a
+                a = b
+                b = temp + b
+                num += 1
+            return b
