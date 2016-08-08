bac8fbc61110697c2233b03cace2d0e42534195b
Star <tingxiaz@andrew.cmu.edu>
2016-08-08 22:19:19 +0800

    [203. Remove Linked List Elements][Runtime Error]committed by LeetCode Extension

---
+++ 203-Remove-Linked-List-Elements/solution.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if head:
+            while head.next != None:
+                head = head.next
+                if head.val == val:
+                    head.val = head.next.val
+                    head.next = head.next.next
+                    
