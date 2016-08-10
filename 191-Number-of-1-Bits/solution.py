0e8c08e5a1d218558ab8896397a68603c0e87e00
Star <tingxiaz@andrew.cmu.edu>
2016-08-10 23:53:12 +0800

    [191. Number of 1 Bits][Runtime Error]committed by LeetCode Extension

---
+++ 191-Number-of-1-Bits/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        str = str(n)
+        count = 0
+        for i in str:
+            if i != 0:
+                count += 1
+        return count
