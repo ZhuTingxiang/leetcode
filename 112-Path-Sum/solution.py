04673b72eec4db343ec35bb8980d2c1b99240452
Star <tingxiaz@andrew.cmu.edu>
2016-08-10 23:29:26 +0800

    [112. Path Sum][Runtime Error]committed by LeetCode Extension

--- 112-Path-Sum/solution.py
+++ 112-Path-Sum/solution.py
@@ -16,7 +16,6 @@ class Solution(object):
             return False
         if root.left or root.right:
             return self.hasPatchSum(root.left, sum-root.val) or self.hasPatchSum(root.right, sum-root.val)
-        else:
-            return False
+        return root,val == sum
             
         
