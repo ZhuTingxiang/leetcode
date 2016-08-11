12c9457313d3a387f9673152802f258bdec9b583
Star <tingxiaz@andrew.cmu.edu>
2016-08-11 18:46:47 +0800

    [50. Pow(x, n)][Runtime Error]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.py
+++ 50-Pow(x,-n)/solution.py
@@ -8,7 +8,7 @@ class Solution(object):
         if n < 0:
             return 1.0 /self.myPow(x,-n)
         if n%2 == 0:
-            return  (self.myPow(x,n/2)) ** 2
+            return  (self.myPow(x,n//2)) ** 2
         else:
             return x * self.myPow(x,n-1)
                 
