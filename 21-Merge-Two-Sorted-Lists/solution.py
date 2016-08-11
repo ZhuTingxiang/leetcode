5e4dbba031582fbbc2a8478b8c0f8f2d3f0eb43b
Star <tingxiaz@andrew.cmu.edu>
2016-08-11 20:35:25 +0800

    [21. Merge Two Sorted Lists][Accepted]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.py
+++ 21-Merge-Two-Sorted-Lists/solution.py
@@ -11,32 +11,15 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        if l1 is not ListNode and l2 is not ListNode:
-            return []
         l3 = temp = ListNode(0)
-        
-        while l1 is not ListNode and l2 is ListNode
-            return l2
-        while l1 is ListNode and l2 is not ListNode:
-            return l1
-        while l1.next is not None and l2.next is not None:
-            print "both"
+        while l1 and l2:
             if l1.val >l2.val:
-                l3.next = l1
-                l1 = l1.next
-            else:
                 l3.next = l2
                 l2 = l2.next
+            else:
+                l3.next = l1
+                l1 = l1.next
             l3 = l3.next
-        while l1.next is not None:
-            print "l1 not None"
-            l3.next = l1
-            l1 = l1.next
-            l3 = l3.next
-        while l2.next is not None:
-            print "l2 is not None"
-            l3.next = l2
-            l2 = l2.next
-            l3 = l3.next
+        l3.next = l1 or l2
         return temp.next
             
