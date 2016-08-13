11f1164959628a41e8bb478d769aef45b43c03ef
Star <tingxiaz@andrew.cmu.edu>
2016-08-14 00:30:26 +0800

    [350. Intersection of Two Arrays II][Runtime Error]committed by LeetCode Extension

--- 350-Intersection-of-Two-Arrays-II/solution.py
+++ 350-Intersection-of-Two-Arrays-II/solution.py
@@ -2,6 +2,8 @@ class Solution(object):
     def intersect(self, nums1, nums2):
         d = dict()
         result = []
+        if nums1 == [] or nums2 == []:
+            return result
         for i in nums1:
             if i not in dict:
                 dict[i] = 1
