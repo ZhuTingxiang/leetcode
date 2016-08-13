35b3f3bc7e5e3f2cbf7a616b4045cdaed0eb5084
Star <tingxiaz@andrew.cmu.edu>
2016-08-13 20:19:29 +0800

    [28. Implement strStr()][Runtime Error]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.py
+++ 28-Implement-strStr()/solution.py
@@ -11,8 +11,8 @@ class Solution(object):
         if haystack == "":
             return -1
         else:
-            for index_n,char_n in needle:
-                for index_h,char_h in haystack:
+            for index_n,char_n in enumerate(needle):
+                for index_h,char_h in enumerate(haystack):
                     if char_n == char_h:
                         if haystack[char_h,char_h + len_n + 1] == needle:
                             return char_h
