ba9b987b29d86b0cfb25fbb9bcd10100c807d205
Star <tingxiaz@andrew.cmu.edu>
2016-08-14 12:17:58 +0800

    [299. Bulls and Cows][Runtime Error]committed by LeetCode Extension

--- 299-Bulls-and-Cows/solution.py
+++ 299-Bulls-and-Cows/solution.py
@@ -14,15 +14,15 @@ class Solution(object):
                         same.append(index2)
                         a += 1
         print same
-         for index1,char1 in enumerate(secret):
-             for index2,char2 in enumerate(guess):
+        for index1,char1 in enumerate(guess):
+            for index2,char2 in enumerate(secret):
                 if char1 == char2:
-                    print char1
-                    if index2 not in same:
-                        if index2 not in almost:
-                            print index2
-                            almost.append(index2)
+                    if index2 not in same and index1 not in same:
+                        if index1 not in almost1 and index2 not in almost2:
+                            almost1.append(index1)
+                            almost2.append(index2)
                             b += 1
+                            
         strr= ""
         strr = str(a)+"A"+str(b)+"B"
         return strr
