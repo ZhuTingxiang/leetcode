0b6f00ef20f91f31368fb1d090f5e5cc65e27fe7
Star <tingxiaz@andrew.cmu.edu>
2016-08-15 14:21:12 +0800

    [24. Swap Nodes in Pairs][Runtime Error]committed by LeetCode Extension

---
+++ 24-Swap-Nodes-in-Pairs/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head:
+            cur = temp = head
+            n = head.next
+            while cur.next:
+                cur.next = cur.next.next
+                n.next = cur
+                cur = cur.next
+                n = cur.next
+            return temp.next
+        return 
+                
+                
+            
