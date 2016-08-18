06f331981fa13afe45cf55f7aa54fe959df8010d
Star <tingxiaz@andrew.cmu.edu>
2016-08-18 09:20:13 +0800

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.py
+++ 205-Isomorphic-Strings/solution.py
@@ -5,15 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        dict = {}
+        dict1,dict2 = {},{}
         for index,char in enumerate(s):
-            if char in dict or t[index] in dict:
-                if dict[char] != t[index]:
+            if char in dict1:
+                if dict1[char] != t[index]:
+                    print "1"
                     return False
             else:
-                # if t[index] in dict:
-                #     return False
-                dict[char] = t[index]
+                dict1[char] = t[index]
+        for index,char in enumerate(t):
+            if char in dict2:
+                if dict2[char] != s[index]:
+                    print "2"
+                    return False
+            else:
+                dict2[char] = s[index]
         return True
         
             
