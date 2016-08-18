ba778ce613747fd165539de4c3869cd512312284
Star <tingxiaz@andrew.cmu.edu>
2016-08-17 18:40:17 -0700

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.py
+++ 205-Isomorphic-Strings/solution.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         dict={}
         for index,char in enumerate(s):
-            if char not in dict1 and t[index] not in dict.values():
+            if char not in dict and t[index] not in dict.values():
                 dict[char] = t[index]
             else:
                 if dict.get(char) != t[index]:
