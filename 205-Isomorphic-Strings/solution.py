71130e6085f3c1bc1a770ef28f4146de99ad271f
Star <tingxiaz@andrew.cmu.edu>
2016-08-17 18:39:52 -0700

    [205. Isomorphic Strings][Runtime Error]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.py
+++ 205-Isomorphic-Strings/solution.py
@@ -8,7 +8,7 @@ class Solution(object):
         dict={}
         for index,char in enumerate(s):
             if char not in dict1 and t[index] not in dict.values():
-                dict1[char] = t[index]
+                dict[char] = t[index]
             else:
                 if dict.get(char) != t[index]:
                     return False
