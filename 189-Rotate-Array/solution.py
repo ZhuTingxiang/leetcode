01b454d7666f93c810034932308caa73028b29ab
Star <tingxiaz@andrew.cmu.edu>
2016-08-20 00:08:10 -0400

    [189. Rotate Array][Accepted]committed by LeetCode Extension

--- 189-Rotate-Array/solution.py
+++ 189-Rotate-Array/solution.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        if k != 0:
-            k = k % len(nums)
-            print k
-            temp = nums[0:len(nums)-k]
-            print temp
-            nums[0:len(nums)-k] = nums[len(nums)-k:len(nums)]
-            nums[k:len(nums)] = temp
+        
+        k = k % len(nums)
+        print k
+        temp = nums[0:len(nums)-k]
+        print temp
+        nums[0:len(nums)-k] = nums[len(nums)-k:len(nums)]
+        nums[k:len(nums)] = temp
             
             
         
