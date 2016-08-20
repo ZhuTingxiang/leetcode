e763ed5170426a2ddcb8bfa85f11a6848cf32d5f
Star <tingxiaz@andrew.cmu.edu>
2016-08-20 14:56:38 -0400

    [278. First Bad Version][Runtime Error]committed by LeetCode Extension

---
+++ 278-First-Bad-Version/solution.py
@@ -0,0 +1,17 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        last = n
+        for index in range(1,len(n)):
+            if isBadVersion(index-1) == False and isBadVersion(index) == True:
+                return index
+                
+        
