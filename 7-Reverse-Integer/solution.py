3e55ea35bda12992ca326d27ea166d5e8c76325e
Star <tingxiaz@andrew.cmu.edu>
2016-08-26 12:43:02 -0400

    [7. Reverse Integer][Accepted]committed by LeetCode Extension

--- 7-Reverse-Integer/solution.py
+++ 7-Reverse-Integer/solution.py
@@ -9,6 +9,6 @@ class Solution(object):
         if x >= 0:
             result = int(s[::-1])
         else:
-            result =int('-'+s[1:][::-1])
+            result =int('-'+s[:0:-1])
         return result if -2147483647 <= result <= 2147483647 else 0
             
