9ff1392245a4bb0bf4b02f0ff716e3096d2fb142
Star <tingxiaz@andrew.cmu.edu>
2016-08-30 13:17:28 -0400

    [167. Two Sum II - Input array is sorted][Accepted]committed by LeetCode Extension

--- 167-Two-Sum-II---Input-array-is-sorted/solution.py
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.py
@@ -6,12 +6,24 @@ class Solution(object):
         :rtype: List[int]
         """
         res = []
-        for index1,char1 in enumerate(numbers):
-            for index2,char2 in enumerate(numbers[::-1]):
-                if char1+char2 == target and index1 != len(numbers)-1-index2:
-                    res.append(index1+1)
-                    res.append(len(numbers)-index2)
-                    return res
+        # for index1,char1 in enumerate(numbers):
+        #     for index2,char2 in enumerate(numbers[::-1]):
+        #         if char1+char2 == target and index1 != len(numbers)-1-index2:
+        #             res.append(index1+1)
+        #             res.append(len(numbers)-index2)
+        #             return res
+        index1 = 0
+        index2 = len(numbers)-1
+        while index1!=len(numbers) and index2!=-1:
+            if numbers[index1] + numbers[index2] == target:
+                res.append(index1+1)
+                res.append(index2+1)
+                return res
+            if numbers[index1] +  numbers[index2] > target:
+                index2 -= 1
+            else:
+                index1 += 1
+            
                     
             
         
