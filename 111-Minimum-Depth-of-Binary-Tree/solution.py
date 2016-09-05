773c392597779ea3561ad9a2b4e58dfcec5a2952
Star <tingxiaz@andrew.cmu.edu>
2016-09-04 21:25:28 -0400

    [111. Minimum Depth of Binary Tree][Runtime Error]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.py
+++ 111-Minimum-Depth-of-Binary-Tree/solution.py
@@ -11,13 +11,42 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        # recursive solution
+        
+        # if not root:
+        #     return 0
+        # if root.right is None:
+        #     return self.minDepth(root.left)+1
+        # if root.left is None:
+        #     return self.minDepth(root.right)+1
+        # else:
+        #     return min(self.minDepth(root.left),self.minDepth(root.right))+1
+        
+        #iterative solution
         if not root:
             return 0
-        if root.right is None:
-            return self.minDepth(root.left)+1
-        if root.left is None:
-            return self.minDepth(root.right)+1
-        else:
-            return min(self.minDepth(root.left),self.minDepth(root.right))+1
+        level = []
+        depth = 1
+        if root.left is not None:
+            level.append(root.left)
+        if root.right is not None:
+            level.append(root.right)
+        while len(level) != 0:
+            nextlevel = []
+            depth += 1
+            for i in level:
+                if i.right is not None:
+                    nextlevel.append(i.left)
+                elif i.left is not None:
+                    nextlevel.append(i.right)
+                elif i.right and i.left:
+                    nextlevel.append(i.right)
+                    nextlevel.append(i.left)
+                else:
+                    nextlevel = []
+            level = nextlevel 
+        return depth
+            
+            
         
         
