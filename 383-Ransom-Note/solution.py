90f704feef2594269c0dd3da0c38c67601dbca4e
Star <tingxiaz@andrew.cmu.edu>
2016-09-07 18:05:46 -0400

    [383. Ransom Note][Accepted]committed by LeetCode Extension

--- 383-Ransom-Note/solution.py
+++ 383-Ransom-Note/solution.py
@@ -5,20 +5,13 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        indexlist =[]
-        if not magazine and ransomeNote:
-            return False
-        for index,char in enumerate(ransomNote):
-            for index2,char2 in enumerate(magazine):
-                if char == char2:
-                    if index2 in indexlist:
-                        if index2 is not len(magazine)-1:
-                            continue
-                        else:
-                            return False
-                    else:
-                        indexlist.append(index2)
-                        break
-                else:
-                    return False
+        used = []
+        
+        magazine = list(magazine)
+        for i in ransomNote:
+            if i in magazine:
+                used.append(i)
+                magazine.remove(i)
+            else:
+                return False
         return True
