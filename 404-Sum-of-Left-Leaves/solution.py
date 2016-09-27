7300ebdb998d0d69a1142c20027647432fd93525
Star <tingxiaz@andrew.cmu.edu>
2016-09-27 10:12:29 -0400

    [404. Sum of Left Leaves][Accepted]committed by LeetCode Extension

--- 404-Sum-of-Left-Leaves/solution.py
+++ 404-Sum-of-Left-Leaves/solution.py
@@ -14,14 +14,11 @@ class Solution(object):
         sum = 0
         if not root:
             return 0
-       
-        if self.sumOfLeftLeaves(root.right) !=0:
-                sum += self.sumOfLeftLeaves(root.right)
-        if self.sumOfLeftLeaves(root.left) !=0:
-                sum += self.sumOfLeftLeaves(root.left)
-        elif root.left:
+        if root.left:
             if root.left.left is None and root.left.right is None:
                 sum += root.left.val
+        sum += self.sumOfLeftLeaves(root.right) + self.sumOfLeftLeaves(root.left)
+        
         return sum
 
             
