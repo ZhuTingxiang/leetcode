fdb8c5156dd0a8bda715e6d10ffba9efe191c5e5
Star <tingxiaz@andrew.cmu.edu>
2016-09-27 10:07:46 -0400

    [404. Sum of Left Leaves][Time Limit Exceeded]committed by LeetCode Extension

--- 404-Sum-of-Left-Leaves/solution.py
+++ 404-Sum-of-Left-Leaves/solution.py
@@ -14,14 +14,14 @@ class Solution(object):
         sum = 0
         if not root:
             return 0
-        if root.left:
-            if root.left.left is None and root.left.right is None:
-                sum += root.left.val
-            
+       
         if self.sumOfLeftLeaves(root.right) !=0:
                 sum += self.sumOfLeftLeaves(root.right)
         if self.sumOfLeftLeaves(root.left) !=0:
                 sum += self.sumOfLeftLeaves(root.left)
+        elif root.left:
+            if root.left.left is None and root.left.right is None:
+                sum += root.left.val
         return sum
 
             
